11bac878179a1a23cba9d09d98e1a45ae067492c
Filipstrozik <filipshelby@gmail.com>
2022-10-20 19:44:06 +0200

    first touch with nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {} //dependecy injection jak w springu
+
+  @Get() // metody odpowiendio
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
